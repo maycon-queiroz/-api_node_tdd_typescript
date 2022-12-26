f6146d11e8f6c6d33f2ca2323a077ef510ce4951
Maycon Queiroz <fmaycon@gmail.com>
2022-12-26 09:36:42 -0300

    init project

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+	preset: '@shelf/jest-mongodb',
+	// collectCoverage: true,
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
+	coverageProvider: 'v8',
+	verbose: true
+};
