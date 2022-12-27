27dff1be0e31bc4a110f0cdcb4e57e17d3f6d080
Maycon Queiroz <fmaycon@gmail.com>
2022-12-26 22:41:10 -0300

    refactor(eventstatus): change EventStatus to class and move rules return status to class EventStatus

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,6 +1,26 @@
 import { set, reset } from 'mockdate'
 
-type EventStatus = { status: string }
+class EventStatus {
+  status: 'active' | 'inReview' | 'done'
+
+  constructor(event?: { endDate: Date, reviewDurationInHours: number }) {
+    if (event === undefined) {
+      this.status = 'done'
+      return;
+    }
+
+    const now = new Date()
+
+    if (event.endDate >= now) {
+      this.status = 'active'
+      return;
+    }
+    const reviewDurationInMiliSeconds = event.reviewDurationInHours * 60 * 60 * 1000
+    const reviewDate = new Date(event.endDate.getTime() + reviewDurationInMiliSeconds)
+
+    this.status = reviewDate >= now ? 'inReview' : 'done'
+  }
+}
 
 class CheckLastEventStatus {
   constructor(private readonly loadLastEventRepository: LoadLastEventRepository) { }
@@ -8,15 +28,7 @@ class CheckLastEventStatus {
   async perform({ groupId }: { groupId: string }): Promise<EventStatus> {
     const event = await this.loadLastEventRepository.loadLastEvent({ groupId })
 
-    if (event === undefined) return { status: 'done' }
-
-    const now = new Date()
-
-    if (event.endDate >= now) return { status: 'active' }
-    const reviewDurationInMiliSeconds = event.reviewDurationInHours * 60 * 60 * 1000
-    const reviewDate = new Date(event.endDate.getTime() + reviewDurationInMiliSeconds)
-
-    return reviewDate >= now ? { status: 'inReview' } : { status: 'done' }
+    return new EventStatus(event)
   }
 }
 
