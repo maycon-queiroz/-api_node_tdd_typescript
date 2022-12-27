c19b6e0ce814b942bd56e1d566277b12aa9c5459
Maycon Queiroz <fmaycon@gmail.com>
2022-12-26 21:04:21 -0300

    feat(checklasteventstatus): ensure return done if loadLastEventRepository return undefined

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,8 +1,9 @@
 class CheckLastEventStatus {
   constructor(private readonly loadLastEventRepository: LoadLastEventRepository) { }
 
-  async perform(groupId: string): Promise<void> {
+  async perform(groupId: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupId)
+    return 'done'
   }
 }
 
@@ -10,24 +11,37 @@ interface LoadLastEventRepository {
   loadLastEvent(groupId: string): Promise<void>;
 }
 
-class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
   groupId?: string
   callsCount = 0
+  output: undefined;
 
-  async loadLastEvent(groupId: string): Promise<void> {
+  async loadLastEvent(groupId: string): Promise<undefined> {
     this.groupId = groupId;
     this.callsCount++
+    return this.output
   }
 }
 
+
 describe('CheckLastEventStatus', () => {
   it('Should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock()
-    const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository)
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    const sut = new CheckLastEventStatus(loadLastEventRepository)
 
-    await checkLastEventStatus.perform('any_group_id')
+    await sut.perform('any_group_id')
 
     expect(loadLastEventRepository.groupId).toBe('any_group_id')
     expect(loadLastEventRepository.callsCount).toBe(1)
+  })
+
+  it('Should return status done', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    loadLastEventRepository.output = undefined
+    const sut = new CheckLastEventStatus(loadLastEventRepository)
+
+    const status = await sut.perform('any_group_id')
+
+    expect(status).toBe('done')
   })
 })
